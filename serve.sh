e8dc27db103f7284bbcd4779d21130ccfd0f8881
alshoja <alshoja@gmail.com>
2023-03-08 15:22:33 +0530

    fix: skelton

--- serve.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo Starting UserService.................
-docker-compose exec app rm -rf vendor composer.lock &&
-
-  
-echo Starting UserService.................
-docker-compose exec app rm -rf vendor composer.lock &&
---
+++ start.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo Starting TodoService.................
+cd TodoService 
+npm run start 
+
+cd ../
+
+echo Starting UserService.................
+cd UserService 
+npm run start 
